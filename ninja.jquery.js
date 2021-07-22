a99b0467765bae2ac9108be6e34cc082c0647b42
Plau2011 <84698212+Plau2011@users.noreply.github.com>
2021-07-21 23:16:14 -0400

    Add files via upload

---
+++ ninja.jquery.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $("#imagen1").click(function() {
+    $("#imagen1").hide()
+})
+$("#imagen2").click(function() {
+    $("#imagen2").hide()
+})
+$("#imagen3").click(function() {
+    $("#imagen3").hide()
+})
+$("#imagen4").click(function() {
+    $("#imagen4").hide()
+})
+$("#imagen5").click(function() {
+    $("#imagen5").hide()
+})
+$("#imagen6").click(function() {
+    $("#imagen6").hide()
+})
+$("#imagen7").click(function() {
+    $("#imagen7").hide()
+})
+$("#imagen8").click(function() {
+    $("#imagen8").hide()
+})
+$("button").click(function(){
+    $("img").show();
+    })
+})
